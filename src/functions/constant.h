c9803d0d4e41bd833bf6fe96e2d61b38f8f0dc4f
Cody Fagley <cody.fagley@gmail.com>
2019-02-13 21:54:54 -0700

    Added Functions and Constants Stubs

---
+++ src/functions/constant.h
@@ -0,0 +1,23 @@
+/*
+  constant.h
+  Cody Fagley
+  Authored on   February 13, 2019
+  Last Modified February 13, 2019
+*/
+
+/*
+  Contains Operations for Constants
+*/
+
+#ifndef FUNCTIONS_CONSTANTS_H
+#define FUNCTIONS_CONSTANTS_H
+
+extern char** ident_constants;
+
+
+
+
+
+
+
+#endif
---
+++ src/functions/functions.h
@@ -0,0 +1,20 @@
+/*
+  functions.h
+  Cody Fagley
+  Authored on   February 13, 2019
+  Last Modified February 13, 2019
+*/
+
+/*
+  Contains Operations for Functions
+*/
+
+#ifndef XCS_FUNCTIONS_H
+#define XCS_FUNCTIONS_H
+
+extern char** ident_functions;
+
+
+
+
+#endif
