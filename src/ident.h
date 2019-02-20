46961e85da366b97e1addac48e9a38a543109855
Cody Fagley <cody.fagley@gmail.com>
2019-02-20 00:10:46 -0700

    Added Stubs for Identifier Resolution

--- src/ident.h
+++ src/ident.h
@@ -7,16 +7,56 @@
 
 /*
   Contains Backend Functionality for Identifier Resolution
+
+  Order of Resolution
+  ===================
+  * Parameter
+  * Function
+  * Constant
+
+
+  Table of Contents
+  =================
+  1.) Resolve Identifier
+    1.a) Check for Parameter
+    1.b) Check for Function
+    1.c) Check for Constant
 */
 
 #ifndef XCS_IDENT_H
 #define XCS_IDENT_H
 
+#include "functions/functions.h"
 
+/*
+  1.) Resolve Identifier
+
+  Returns:
+    0, if unable to resolve ident
+    1, if ident is a Parameter
+    2, if ident is a Function
+    3, if ident is a Constant
+*/
 int resolve_identifier(char* ident)
 {
-  printf("Identifier Resolved: %s\n", ident);
+  int rtn;
 
+  //  1.a) Check for Parameter
+  rtn = find_parameter(ident);
+  if (rtn) { printf("Identifier is Parameter\n"); return 1; }
+
+
+  //  1.b) Check for Function
+  rtn = find_function(ident);
+  if (rtn) { printf("Identifier is Function\n"); return 2; }
+
+  
+  //  1.c) Check for Constant
+  rtn = find_constant(ident);
+  if (rtn) { printf("Identifier is Constant\n"); return 3; }
+
+
+  printf("Unable to resolve Identifier: %s\n", ident);
   return 0;
 }
 
