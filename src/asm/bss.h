13ff062e6e62ed3d962d1c7b619912119e2c6d7f
Cody Fagley <cody.fagley@gmail.com>
2019-02-20 00:57:29 -0700

    Added Stubs for .bss & .data Sections

--- src/asm/bss.h
+++ src/asm/bss.h
@@ -6,21 +6,52 @@
 */
 
 /*
-  .bss Section Contains Uninitialized Data
+  .bss Section Contains Uninitialized Data (Variables)
+
+  Table of Contents
+  =================
+  1.) Structure
+    1.a) Generate BSS
+  2.) Operations
+    2.a) Add to BSS
 */
 
 #ifndef ASM_BSS_H
 #define ASM_BSS_H
 
+
 /*
-  Adds BSS Command to .bss Section of Assembly File
+  1.) Structure
 */
-int add_bss(char* command)
+
+/* 1.a) Generate BSS
+
+  Returns:
+    0, if Successful
+*/
+int generate_bss()
+{
+
+  return 0;
+}
+
+/*
+  2.) Operations
+*/
+
+/* 2.a) Add to BSS
+  Adds BSS Command to .bss Section of Assembly File
+
+  Returns:
+    0, if Successful
+*/
+int add_to_bss(char* ident, unsigned int num_bytes)
 {
   //  STUB STUB STUB STUB
 
   return 0;
 }
+
 
 
 #endif
--- src/asm/data.h
+++ src/asm/data.h
@@ -6,12 +6,116 @@
 */
 
 /*
-  Contains backend assembly infrastructure for .data section
+  .data Section contains Initialized Data (Constants)
+
+  Table of Contents
+  =================
+  1.) Structure
+    1.a) Generate Data
+  2.) Operations
+    2.a) Add Integer to Data
+    2.b) Add Real to Data
+    2.c) Add Boolean to Data
+    2.d) Add Character to Data
+    2.e) Add String to Data
+    2.f) Add List to Data
 */
 
 #ifndef ASM_DATA_H
 #define ASM_DATA_H
 
+/*
+  1.) Structure
+*/
 
+/* 1.a) Generate Data
+
+  Returns:
+    0, if Successful
+*/
+int generate_data()
+{
+
+  return 0;
+}
+
+/*
+  2.) Operations
+*/
+
+/* 2.a) Add Integer to Data
+Adds Integer Constant to .data Section
+
+  Returns:
+    0, if Successful
+*/
+int add_int_to_data(char* ident, int value)
+{
+  //  STUB STUB STUB STUB
+
+  return 0;
+}
+
+/* 2.b) Add Real to Data
+Adds Real Constant to .data Section
+
+  Returns:
+    0, if Successful
+*/
+int add_real_to_data(char* ident, double value)
+{
+  //  STUB STUB STUB STUB
+  
+  return 0;
+}
+
+/* 2.c) Add Boolean to Data
+
+  Returns:
+    0, if Successful
+*/
+int add_bool_to_data(char* ident, bool value)
+{
+  //  STUB STUB STUB STUB
+
+  return 0;
+}
+
+/* 2.d) Add Character to Data
+
+  Returns:
+    0, if Successful
+*/
+int add_char_to_data(char* ident, bool value)
+{
+  //  STUB STUB STUB STUB
+
+  return 0;
+}
+
+/* 2.e) Add String to Data
+
+  Returns:
+    0, if Successful
+*/
+int add_string_to_data(char* ident, char* str)
+{
+  //  STUB STUB STUB STUB
+
+  return 0;
+}
+
+/* 2.f) Add List to Data
+
+NOTE: Constant Lists are only available in Linux-to-XCS Cross Compiler
+  Returns:
+    0, if Successful
+*/
+int add_list_to_data(char* ident, long value)
+{
+  //  STUB STUB STUB STUB
+
+  return 0;
+}
 
 #endif
