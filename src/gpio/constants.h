e58b4746b2205afaf81e348555820d17942644a0
Cody Fagley <cfagley@codeus.tech>
2019-02-22 10:32:59 -0700

    added Stub for Real-Time Clocks

--- src/gpio/constants.h
+++ src/gpio/constants.h
@@ -20,12 +20,12 @@
 /*
   1.) Peripheral Memory Buses
 */
-int CS0 = 0x00000000;
-int CS1 = 0x14000000;
-int CS2 = 0x18000000;
-int CS3 = 0x1C000000;
-int CS4 = 0x0C000000;
-int CS5 = 0x10000000;
+const unsigned int CS0 = 0x00000000;
+const unsigned int CS1 = 0x14000000;
+const unsigned int CS2 = 0x18000000;
+const unsigned int CS3 = 0x1C000000;
+const unsigned int CS4 = 0x0C000000;
+const unsigned int CS5 = 0x10000000;
 
 
 
---
+++ src/gpio/rtc.h
@@ -0,0 +1,39 @@
+/*
+  rtc.h
+  Cody Fagley
+  Authored on   February 22, 2019
+  Last Modified February 22, 2019
+*/
+
+/*
+  Contains Structures/Operations for IO FPGA Real-Time Clock
+
+
+  Reference/Documentation
+  =======================
+  https://static.docs.arm.com/ddi0224/c/real_time_clock_pl031_r1p3_technical_reference_manual_DDI0224C.pdf
+
+
+  Table of Contents
+  =================
+  1.) Structures/Constants
+  2.) Operations
+*/
+
+#ifndef GPIO_RTC_H
+#define GPIO_RTC_H
+
+#include "constants.h"
+
+/*
+  1.) Structures/Constants
+*/
+
+const unsigned int rtc_base =  CS3 + 0x17000;
+
+
+/*
+  2.) Operations
+*/
+
+#endif
